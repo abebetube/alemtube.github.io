be382cc864c142b919259f7174af5ef9bb2d57a7
abebe <abebe59@gmail.com>
2026-01-29 18:07:30 +0200

    Update public API key in config.js
    Signed-off-by: abebe <abebe59@gmail.com>

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
  */
 
 // המפתח שלך (אם עובד)
-const YOUR_API_KEY = "AIzaSyComUEoEhXH-wGzbJRIoVnlajCO4AiVrLk";
+const YOUR_API_KEY = "AIzaSyCaKQjUG_XZmKFoD4_hKfMRew9h_UTOzN4";
 
 // מפתח גיבוי ציבורי (מוגבל)
 const BACKUP_API_KEY = "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8";
