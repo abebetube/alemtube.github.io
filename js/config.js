6d3ff23ee0d82d6e8900564feef0579fbf9be8c4
abebe <abebe59@gmail.com>
2026-01-29 17:07:27 +0200

    Rename config file and simplify settings
    Updated the configuration file name and removed unused settings.
    
    Signed-off-by: abebe <abebe59@gmail.com>

--- js/config.js
+++ js/config.js
@@ -1,28 +1,12 @@
 /**
- * קובץ הגדרות AbeTube
+ * קובץ הגדרות AlemTube
  * ⚠️ חשוב: קובץ זה מכיל מידע רגיש - אל תשתף אותו בפומבי!
  */
 
 // מפתח YouTube Data API v3
-// להחלפה במפתח שלך: https://console.cloud.google.com/apis/credentials
 const YOUTUBE_API_KEY = "AIzaSyCKWg2Po9gpQTx2-SSadDOouTB04jBFAAU";
 
-// הגדרות נוספות
-const APP_CONFIG = {
-    apiKey: YOUTUBE_API_KEY,
-    maxResults: 30,           // מספר תוצאות מקסימלי
-    defaultQuality: "hd720",  // איכות ברירת מחדל
-    autoPlayNext: true,       // ניגון אוטומטי לסרטון הבא
-    language: "he",           // שפת תוצאות
-    regionCode: "IL"          // קוד אזור (ישראל)
-};
+// הגדרה גלובלית לשימוש בקובץ alemtube.js
+window.YOUTUBE_API_KEY = YOUTUBE_API_KEY;
 
-// יצוא ההגדרות
-if (typeof module !== 'undefined' && module.exports) {
-    // עבור Node.js/CommonJS
-    module.exports = APP_CONFIG;
-} else {
-    // עבור דפדפן
-    window.APP_CONFIG = APP_CONFIG;
-    window.YOUTUBE_API_KEY = YOUTUBE_API_KEY;
-}
+console.log("✅ קובץ config.js נטען בהצלחה");
