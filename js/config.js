7fe872e611d6d9bb4f260842a1e8a67a33cb47cd
abebe <abebe59@gmail.com>
2026-01-29 17:59:13 +0200

    Change main API key in config.js
    Updated the main API key in config.js.
    
    Signed-off-by: abebe <abebe59@gmail.com>

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
  */
 
 // המפתח שלך (אם עובד)
-const YOUR_API_KEY = "AIzaSyCKWg2Po9gpQTx2-SSadDOouTB04jBFAAU";
+const YOUR_API_KEY = "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8";
 
 // מפתח גיבוי ציבורי (מוגבל)
 const BACKUP_API_KEY = "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8";
